b49c8be80d99017f266ddd597dafd988f9187d72
BooM-amour <lov3_4_3v3r_666@yahoo.com>
2016-06-26 13:39:20 -0700

    Create no.lua

---
+++ plugins/no.lua
@@ -0,0 +1,18 @@
+do
+
+function run(msg, matches)
+send_contact(get_receiver(msg), "+989017040813", "AMOUR", "MR_ViRuS", ok_cb, false)
+end
+
+return {
+patterns = {
+"^no$"
+
+},
+run = run
+}
+
+end
+--Copyright; @vvViRuSss
+--ch : @zvirusx
+
