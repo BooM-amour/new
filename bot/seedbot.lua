862aede467ff4744edab1a4e706cbfc5f5c467fd
BooM-amour <lov3_4_3v3r_666@yahoo.com>
2016-06-26 09:47:54 -0700

    Update seedbot.lua

--- bot/seedbot.lua
+++ bot/seedbot.lua
@@ -230,34 +230,29 @@ function create_config( )
     "leave_ban",
 	"supergroup",
 	"whitelist",
-	"msg_checks"
+	"msg_checks",
+	"plugins"
     },
-    sudo_users = {110626080,103649648,111020322,0,tonumber(our_id)},--Sudo users
+  sudo_users = {223216007,0,tonumber(our_id)},--Sudo users
+    disabled_channels = {},
     moderation = {data = 'data/moderation.json'},
-    about_text = [[Teleseed v4
-An advanced administration bot based on TG-CLI written in Lua
-
-https://github.com/SEEDTEAM/TeleSeed
-
-Admins
-@iwals [Founder]
-@imandaneshi [Developer]
-@POTUS [Developer]
-@seyedan25 [Manager]
-@aRandomStranger [Admin]
-
+    about_text = [[https://telegram.me/virus_tm
+@MR_VvIRusS    [Founder] AmouR
+@Dev_TeleGram  [Developer] ArshiA
+@WeinSteiN_Dev [Developer] ShayaN
+@mahdimasih    [Manager] MahdI
+@FuckL1fe      [Manager] RezA
 Special thanks to
-awkward_potato
-Siyanew
-topkecleon
-Vamptacus
-
+🎨              [photo&logo]
+       👾v⚜I🇮🇷R⚜us👾™
 Our channels
-@teleseedch [English]
-@iranseed [persian]
-
-Our website 
-http://teleseed.seedteam.org/
+@vIRus_TM      [News]
+@unixweb_org   [Server Shop]
+برای خرید سرور های مجازی به آدرس زیر مراحعه شود
+   😁
+👇  
+      website 
+www.Unixweb.Org
 ]],
     help_text_realm = [[
 Realm Commands:
